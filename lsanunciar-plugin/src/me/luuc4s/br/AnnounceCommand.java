164667613d8b3b73d738b6e1b52410f990c22d3e
Lucas Lima <113833346+Luuc4sBR@users.noreply.github.com>
2023-12-16 22:42:35 -0300

    Update AnnounceCommand.java

--- lsanunciar-plugin/src/me/luuc4s/br/AnnounceCommand.java
+++ lsanunciar-plugin/src/me/luuc4s/br/AnnounceCommand.java
@@ -13,21 +13,24 @@ public class AnnounceCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
 		Player p = (Player) sender;
-		
+
 		if (p.hasPermission("anunciar.usar")) {
 			if (cmd.getName().equalsIgnoreCase("anunciar")) {
 				if (sender instanceof Player) {
 					if (args.length > 0) {
-					String message = String.join(" ", args).replace("&", "§");	
-					Bukkit.broadcastMessage((Main.main.getConfig().getString("Chat.Formato-mensagem").replace("&", "§").replace("%player%", p.getName())).replace("%mensagem%", message));
-				} else {
-					sender.sendMessage(ChatColor.GREEN + "Utilize: '/anunciar <mensagem>' para estar anunciando alguma mensagem.");
-				}} else {
-				sender.sendMessage(ChatColor.RED + "Apenas jogadores podem usar este comando.");
-			} return true;
-		}} else {
-		sender.sendMessage(ChatColor.RED + "Você não tem permissão para utilizar este comando.");
-		return true;
+						String message = String.join(" ", args).replace("&", "Â§");
+						Bukkit.broadcastMessage((Main.main.getConfig().getString("Chat.Formato-mensagem")
+								.replace("&", "Â§").replace("%player%", p.getName())).replace("%mensagem%", message));
+					} else {
+						sender.sendMessage(ChatColor.GREEN
+								+ "Utilize: '/anunciar <mensagem>' para estar anunciando alguma mensagem.");
+					}} else {
+					sender.sendMessage(ChatColor.RED + "Apenas jogadores podem usar este comando.");
+				}
+				return true;
+			}} else {
+			sender.sendMessage(ChatColor.RED + "VocÃª nÃ£o tem permissÃ£o para utilizar este comando.");
+			return true;
 		}
 		return false;
 	}}
